2d2440ba710f907e070de7c56521c3c47909d399
laian <laian41@gmail.com>
2014-06-02 13:28:09 -0400

    cleaned up comments

--- src/OutputVariable.scala
+++ src/OutputVariable.scala
@@ -33,25 +33,12 @@ object OutputVariable {
 	def populate(tableNameVec : Vector[String])  = {
 		
 		tableNames = tableNameVec
-		//val temp = OutputDesiredVariable.all
-		//pKeys = Vector(Utility.getPrimaryKeys(tableNames(0)),
-		//		Utility.getPrimaryKeys(tableNames(1)))
 		keys = for (attr <- OutputDesiredVariable.all) yield{
 			attr.tableName + "." + attr.attrName // Temporarily using
 		}
 		val OutputVariables : ArrayBuffer[OutputVariable] = ArrayBuffer()
-		
-//		val resultSet1 = Utility.selectAllColumns(tableNames(0), pKeys(0))
-//		while(resultSet1.next()) {
-//			val resultSet2 = Utility.selectAllColumns(tableNames(1), pKeys(1))
-//			while(resultSet2.next()) {
-//				OutputVariables += new OutputVariable(Utility.resultToVector(resultSet1), 
-//						Utility.resultToVector(resultSet2))
-//			}
-//		}
 		val resultSet1 = Utility.selectAllColumns(tableNames, keys.toVector)
 		while(resultSet1.next()) {
-		  //println(Utility.resultToVector(resultSet1))
 		  OutputVariables += new OutputVariable(Utility.resultToVector(resultSet1)) 
 		}
 		otvs = OutputVariables.toVector
