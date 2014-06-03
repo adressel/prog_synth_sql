108b931a75ac9632c9f3983077c48b41cc72fe52
laian <laian41@gmail.com>
2014-06-03 10:57:06 -0400

    huh

--- src/printer.scala
+++ src/printer.scala
@@ -2,11 +2,11 @@ package src
 import java.io._
 
 object printer {
-	private	val root = "./" // for Ian
+	private	val root = "./sat/cnf_files/" // for Ian
 //	private val root = "/Users/Stephen/Desktop/FeatureCreature" // for Sheng
 	
 	def printFile = {
-		val out = new PrintWriter(s"${root}results.txt")
+		val out = new PrintWriter(s"${root}output.cnf")
 		val header = "c output.enc\nc\np cnf "+ Variable.count +" " + Clause.clauses.length + " \n"
 		out.print(header)
 		for (clause <- Clause.clauses){
@@ -14,7 +14,6 @@ object printer {
 		  for (attr <- clause.literals){
 			  cnf += ((if(!attr._2) {"-"} else {""}) +  attr._1.id + " ")
 		  }
-		  //println(cnf + "0\n")
 		  out.print(cnf + "0\n") 
 		}
 		out.close()
