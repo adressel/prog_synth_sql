f3f1a421348f7e1cb71b3f21c35d3426016cf217
lslsheng <527873752@qq.com>
2014-06-03 11:45:04 -0400

    printer

--- src/File.scala
+++ src/File.scala
@@ -13,19 +13,24 @@ object printer {
 		val header = "c output.enc\nc\np cnf "+ Variable.count +" " + Clause.clauses.length + " \n"
 		out.print(header)
 		for (clause <- Clause.clauses){
-		  var cnf = ""
-		  for (attr <- clause.literals){
-			  cnf += ((if(!attr._2) {"-"} else {""}) +  attr._1.id + " ")
+		  val ruleNum : Int = clause._2 
+		  out.print(s"c =========  rule $ruleNum  ============\n") 
+		  for (rules <- clause._1){
+			  var cnf = ""
+			  for (attr <- rules.literals){
+				  cnf += ((if(!attr._2) {"-"} else {""}) +  attr._1.id + " ")
+			  }
+			  out.print(cnf + "0\n") 
 		  }
-		  out.print(cnf + "0\n") 
+		  
 		}
 		out.close()
 	}
 }
 
 object Reader {
-	private	val root = "./sat/cnf_files/" // for Ian
-//	private val root = "/Users/Stephen/Desktop/FeatureCreature" // for Sheng
+//	private	val root = "./sat/cnf_files/" // for Ian
+	private val root = "/Users/Stephen/Desktop/FeatureCreature" // for Sheng
 		
 	def readFile = {
 		val in = Source.fromFile(s"${root}output.cnf").mkString
