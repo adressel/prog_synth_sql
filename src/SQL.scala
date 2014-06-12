a1161d3157bc693ae1c49a79876dddb488b0094c
laian <laian41@gmail.com>
2014-06-12 14:30:42 -0400

    cleanup .sql

--- src/SQL.scala
+++ src/SQL.scala
@@ -7,8 +7,8 @@ object SQL {
   
   val root = s"${Data.root}sql"
   
-  val num_users = 50
-  val num_albums = 50
+  val num_users = 100
+  val num_albums = 100
   val num_photos = 100
   val num_contains = 100
 
