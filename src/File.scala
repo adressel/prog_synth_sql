0a1e8f669581b1ccdcf016f743be48eaae57014d
lslsheng <527873752@qq.com>
2014-06-16 13:49:13 -0400

    version 3

--- src/File.scala
+++ src/File.scala
@@ -24,26 +24,46 @@ object Printer {
 //		out.close()
 //	}
   
-  def ian_file = {
+//  def print_file = {
+//	    val outputFile = new File(s"${Data.root}sat/cnf_files/output.cnf")
+//	    val out = new BufferedWriter(new FileWriter(outputFile))
+//		val header = s"c output.enc\nc\np cnf ${Variable.count} ${Clause.size} \n"
+//		out.write(header)
+//		for (clause <- Clause.clauses){
+//		  val ruleNum : Int = clause._2 
+//		  out.write(s"c =========  rule $ruleNum  ============\n") 
+//		  var cnf = new StringBuffer
+//		  for (rules <- clause._1){
+//			  for (attr <- rules.literals){
+//				  cnf.append((if(!attr._2) {"-"} else {""}) +  attr._1 + " ")
+//			  }
+//			  cnf.append("0\n")
+//		  } 
+//		  out.write(cnf.toString)
+//		  out.flush()
+//		}
+//		out.close()
+//	}
+  
+	def print_file = {
 	    val outputFile = new File(s"${Data.root}sat/cnf_files/output.cnf")
-	    val out = new BufferedWriter(new FileWriter(outputFile), 4000000)
+	    val out = new BufferedWriter(new FileWriter(outputFile))
 		val header = s"c output.enc\nc\np cnf ${Variable.count} ${Clause.size} \n"
 		out.write(header)
 		for (clause <- Clause.clauses){
 		  val ruleNum : Int = clause._2 
 		  out.write(s"c =========  rule $ruleNum  ============\n") 
-		  var cnf = new StringBuffer
 		  for (rules <- clause._1){
 			  for (attr <- rules.literals){
-				  cnf.append((if(!attr._2) {"-"} else {""}) +  attr._1 + " ")
+				  out.write((if(!attr._2) {"-"} else {""}) +  attr._1 + " ")
 			  }
-			  cnf.append("0\n")
+			  out.write("0\n")
 		  } 
-		  out.write(cnf.toString)
 		  out.flush()
 		}
 		out.close()
 	}
+  
 }
 
 
