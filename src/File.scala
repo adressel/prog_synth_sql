256dcf7bfb3d80470c06d5b0e4d61cd8cb43ac9b
laian <laian41@gmail.com>
2014-07-22 13:34:50 -0400

    removes Reader from File.scala

--- src/File.scala
+++ src/File.scala
@@ -37,17 +37,3 @@ object Printer {
 		out.close()
 	}
 }
-
-
-object Reader {
-	def process_cnf = {
-		val result = Source.fromFile(s"${Data.root}sat/results/results.txt").mkString
-		val pattern = "(.*)Random Seed Used".r
-		val Some(patternMatch) = pattern.findFirstMatchIn(result)
-		val clauses = patternMatch.group(1).split(" ").filter(_(0) != '-').map(x => x.toInt)
-		val conditions = clauses.map(x => Variable.all(x-1)).filter(_.isInstanceOf[ConditionVariable])
-		val wheres = conditions.map(x => x.print).mkString(" and \n")
-		println("\n======== QUERY ============")
-		println(s"${Data.desired_query} where $wheres")
-	}
-}
